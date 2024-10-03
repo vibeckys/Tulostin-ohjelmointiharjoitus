83c9352cd7001832f88c0622dbeac145fea586b2
vibeckys <amk1005954@student.hamk.fi>
2024-10-03 09:26:14 +0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,61 @@
+public class App {
+    private static int result;
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hei olen Tulostin-ohjelma");
+
+        String Tekija;
+
+        Tekija ="Vibecky";
+        
+        System.out.println("Ohjelman tekijä: " + Tekija);
+         //Esimerkki
+        int luku1;
+        int luku2;
+       
+
+        luku1 = 10;
+        luku2 = 20;
+        luku1 = 5;
+        luku2 = 2;
+        luku1 = 6;
+        int tulo;
+        tulo = luku1 * luku2; 
+        System.out.println(luku1);
+        System.out.println(luku2);
+
+        System.out.println(tulo);
+        System.out.println(luku1 +luku2);
+        
+        //double luku1, luku2, tulo;
+        // luku1=5;
+        // luku2=2;
+        // tulo = luku1 * luku2;
+        
+
+     }
+
+}   
+        
+
+
+
+
+        
+
+        
+
+    
+
+
+
+
+
+
+        
+
+
+
+
+
+
